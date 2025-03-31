8c01eaad56b3c9204051bd0253e801e35efe267d
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-03-31 12:32:33 -0600

    Git init: Initial project config

---
+++ src/vite-env.d.ts
@@ -0,0 +1,25 @@
+/// <reference types="vite/client" />
+
+/// <reference types="vite/client" />
+
+declare module '*.svg' {
+    import React = require('react');
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
+
+declare module '*.jpg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.png' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.mp4' {
+    const content: string;
+    export default content;
+}
