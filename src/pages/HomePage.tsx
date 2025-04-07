88a094607a2dac363df1516aa725b56a5a8be7f8
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-04-07 11:16:29 -0600

    Feat: Add Acordion FreqQuestions Component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,9 +5,12 @@ import InfoSection from '@/components/sections/InfoSection';
 import MapTriumph from '@/components/sections/MapTriumph';
 import MotorcycleShowcase from '@/components/sections/MotorcycleShowcase';
 import RegistrationForm from '@/components/sections/RegistrationForm';
+import TriumphFaqAccordion from '@/components/sections/TriumphFaqAccordion';
 import TriumphExperienceGallery from '@/components/sections/TriumphExperienceGallery';
 import TipSection from '@/components/sections/TIpSection';
 import Footer from '@/components/layout/Footer';
+import RegistrationReminder from '@/components/sections/RegistrationReminder';
+
 
 const HomePage = () => {
     return (
@@ -20,7 +23,9 @@ const HomePage = () => {
             <TriumphExperienceGallery />
             <MapTriumph />
             <RegistrationForm />
+            <TriumphFaqAccordion />
             <TipSection />
+            <RegistrationReminder />
             <Footer />
         </>
     );
