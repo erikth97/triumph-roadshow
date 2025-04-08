31cda45ce179d096da3a786b1feb5c99bdc43ebd
Snozu <hazieleduardo@gmail.com>
2025-04-07 18:29:56 -0600

    Fix: remove CtaSection from HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,12 +9,8 @@ import TriumphFaqAccordion from '@/components/sections/TriumphFaqAccordion';
 import TriumphExperienceGallery from '@/components/sections/TriumphExperienceGallery';
 import TipSection from '@/components/sections/TIpSection';
 import Footer from '@/components/layout/Footer';
-<<<<<<< HEAD
-import CtaSection from '@/components/sections/CtaSection';
-=======
 import RegistrationReminder from '@/components/sections/RegistrationReminder';
 
->>>>>>> db3896563b90c852de2e229a87ba94347a140a8f
 
 const HomePage = () => {
     return (
@@ -29,11 +25,7 @@ const HomePage = () => {
             <RegistrationForm />
             <TriumphFaqAccordion />
             <TipSection />
-<<<<<<< HEAD
-            <CtaSection />
-=======
             <RegistrationReminder />
->>>>>>> db3896563b90c852de2e229a87ba94347a140a8f
             <Footer />
         </>
     );
